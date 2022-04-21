6b7751e01c25e6292c074a46aa7a449aacbabef6
VincentNOURY <vincent.noury@efrei.net>
2022-04-20 19:31:34 -0700

    Removed fake values

--- Assignments/3/database/seed.sql
+++ Assignments/3/database/seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (id, name, email, premium_id)
-VALUES (0, 'John Doe', 'john.doe@example.com', 0),
-       (1, 'Jack Sparrow', 'jack.sparrow@site.com', NULL),
-       (2, 'FirstName LastName', 'firstname.lastname@gmail.com', 1);
+VALUES (0, 'Jack Spencer', 'jack.spencer@gmail.com', 0),
+       (1, 'Jack Sparrow', 'jack.sparrow@gmail.com', NULL),
+       (2, 'Walter White', 'walter.white@gmail.com', 1);
 
 INSERT INTO premium (id, users_id, level, date_start_membership)
 VALUES (0, 0, 1, TO_DATE('20/04/2022', 'DD/MM/YYYY')),
